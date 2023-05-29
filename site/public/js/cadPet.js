e724ff9c4787c4d189532f99660f757743d76f45
annamatos <anna.matos@sptech.school>
2023-05-29 10:54:50 -0300

    Ajustando js

--- site/public/js/cadPet.js
+++ site/public/js/cadPet.js
@@ -1,3 +1,10 @@
 function cadastroPet(){
+    var pet={
+        nome: nomePet.value,
+        dtNasc:dtNascPet.value,
+        tipo: tipoPet.value,
+        raça: raçaPet.value
+    }
+    cadastrado.innerHTML += `${JSON.stringify(pet)}`
 alert(`cadastro dp animal`)
 }
