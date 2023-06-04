4ae59e2031981944129338b8cda7914bdb514e5c
annamatos <anna.matos@sptech.school>
2023-06-04 14:38:10 -0300

    Ajustes na página de histárias

--- site/public/js/historiaSite.js
+++ site/public/js/historiaSite.js
@@ -7,12 +7,40 @@ for (var contador = 0; contador < qntHistoria; contador++) {
     listaHistorias.push(id)
 }
 
-
 var posicao=listaHistorias[0]
 
 function proxima(){
     
     function exibirPeloId() {
+      if (posicao <= listaHistorias.length){
+        fetch(`/historias/${posicao}`, {
+          method: "GET"
+        })
+          .then(res => {
+            res.json().then(json => {
+              const historia = json[0];
+              titulo.innerHTML = `<span>${historia.nome}</span>`
+              corpo.innerHTML =`<span>${historia.descricao}</span>
+              <div class="autor"><span>${historia.nomeAutor}</span></div>`
+              imagem.innerHTML = `<div class="imagem"><img src="../assets/${historia.imagem}" alt=""></div>`
+             
+            })
+          })
+          .catch(err => {
+            console.log(err);
+          })
+          posicao++
+      } else{
+        alert(`deu certo`)
+      }
+    }
+      exibirPeloId()
+}
+function anterios(){
+  posicao = posicao - 1
+    if(posicao > 0){
+      setaL.style.display = 'flex';
+      function exibirPeloId() {
         fetch(`/historias/${posicao}`, {
           method: "GET"
         })
@@ -29,12 +57,31 @@ function proxima(){
           .catch(err => {
             console.log(err);
           })
-          posicao++
+          
       }
       exibirPeloId()
-}
-function anterios(){
-    alert('funcioca')
-    var nome =anna
-    listaHistorias.push(nome)
+
+    } else{
+      posicao=listaHistorias.length
+        function exibirPeloId() {
+          fetch(`/historias/${posicao}`, {
+            method: "GET"
+          })
+            .then(res => {
+              res.json().then(json => {
+                const historia = json[0];
+                titulo.innerHTML = `<span>${historia.nome}</span>`
+                corpo.innerHTML =`<span>${historia.descricao}</span>
+                <div class="autor"><span>${historia.nomeAutor}</span></div>`
+                imagem.innerHTML = ` <img src="../assets/${historia.imagem}" width="200px" alt="">`
+               
+              })
+            })
+            .catch(err => {
+              console.log(err);
+            })
+            
+        }
+        exibirPeloId()
+    }
 }
